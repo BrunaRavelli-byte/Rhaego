f282e6e17d1e909357e85dd32569ba2e32abb435
Bruna Ravelli <brunaravelli@outlook.com>
2022-11-01 16:59:18 -0300

    Alguns ajustes em nomes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 let ano_atual = new Date();
 ano_atual = ano_atual.getFullYear();
 
-document.getElementById("grid-16").innerHTML = "Rhaego - " + ano_atual + " © Todos os direitos reservados"
+document.getElementById("grid-16").innerHTML = `Rhaego - ${ano_atual} © Todos os direitos reservados`
